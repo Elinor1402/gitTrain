7b07cf922bb0539e8b5dbc67256a82d3582fbd2e
BANSHEE\elinorz <elinor.zalogin@comm-it.com>
2025-04-16 14:41:34 +0300

    sourceTree learn commits start

--- ex.py
+++ ex.py
@@ -1,8 +1,2 @@
 print ("hello2")
-
-print("Add this from new user")
-
-print("Added 1st line from techteam1 on GitHub")
-print("Added 2st line from techte
-print("Changed by User-2 accout")
-x=100
+print("Write git train change")
